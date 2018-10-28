1875d64060ff4ed0597db308ddb981f0940ecfb1
Susanna Kline <susannakline@gmail.com>
2018-10-28 16:45:17 -0500

    Add first parts of map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,21 @@
 import React from 'react'
+//Credit to https://tomchentw.github.io/react-google-maps/#installation for instructions on adding react-google-maps
+import { GoogleMap } from "google-maps-react"
+
+
+const mapComponent = (props) => {
+    <GoogleMap
+        defaultZoom={12}
+        defaultCenter={{ lat: 40.117243, lng: -88.240827 }}
+    >
+    </GoogleMap>
+}
 
 const Map = (props) => {
-    return (
-        <div>
-            
-        </div>
+    return(
+    <MapComponent
+    />
+    )}
 
-    )
-}
 
 export default Map
