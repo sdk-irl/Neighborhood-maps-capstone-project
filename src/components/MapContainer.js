0009e00bf7bbf5fbdb22c4bf1049ef61024cf487
Susanna Kline <susannakline@gmail.com>
2018-11-02 22:02:23 -0500

    Fetch and match restaurant data from FourSquare

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react'
-//bootstrapped this app with google-maps-react and now importing necessary items from this API
+// bootstrapped this app with google-maps-react and now importing necessary items from this API
 import {Map, InfoWindow, GoogleApiWrapper} from 'google-maps-react'
 
-//set the API key here to use later and change easier if needed
+// set the API key here to use later and change easier if needed
 const API_KEY = 'AIzaSyAVIlVT1r_WJh4Ru7aIAU8NAd7GPxPtQC8';
-const FourSquare_CLIENT = '';
-const FourSquare_SECRET = '';
-const FourSquare_VERSION = '';
+const FourSquare_CLIENT_ID = 'PVZVSGGBQZNENEBHDC5DW1VCALWA1CCUNLL42ZE0XRZOXNKL';
+const FourSquare_SECRET = 'GKFEBJIWGEZDXWUANQPVQTPOITCVS451ZEJ2YK1CYEGMN3QU';
+// set FourSquare version to date of writing, FourSquare says to update every couple of months to ensure on latest version
+const FourSquare_VERSION = '20181102';
 
 
-//Used this documentation extensively to write this component https://www.npmjs.com/package/google-maps-react
+// Used this documentation extensively to write this component https://www.npmjs.com/package/google-maps-react
 class MapContainer extends Component {
   state = {
       map: null,
@@ -20,19 +21,19 @@ class MapContainer extends Component {
       showingInfoWindow: false
   }
 
-  //when the map is loaded, run this function
+  // when the map is loaded, run this function
   componentDidMount() {
     //TODO
   }
 
-  //when the map is loaded, set the state and fetch the places
+  // when the map is loaded, set the state and fetch the places
   fetchPlaces = (mapProps, map) => {
     this.setState({map});
     this.setState({mapProps});
     this.resetMarkers(this.props.locations);
   }
 
-  //close the info window and make the active marker null when this is called (which is when the next marker is clicked)
+  // close the info window and make the active marker null when this is called (which is when the next marker is clicked)
   hideInfoWindow = () => {
     this.state.selectedMarker && this.state.selectedMarker.setAnimation(null);
     this.setState({
@@ -41,11 +42,51 @@ class MapContainer extends Component {
     })
   }
 
+  // find businesses in foursquare data that match my locations.json
+  findBusinessMatch = (selectedMarkerProps, fsData) => {
+    console.log(selectedMarkerProps, 'fsdata:', fsData);
+    let BusinessMatch = fsData.response.venues.filter(item => 
+      // checking for both restaurant names from the foursquare data restaurant names that include location.json restaurant names, 
+      // or restaurant names from location.json that include the foursquare data restaurant names.
+      // Both are needed because one is sometimes longer than the other in the data.
+      // Does not check for inexact matches except for length. 
+      item.name.includes (selectedMarkerProps.restaurantName) || 
+      selectedMarkerProps.restaurantName.includes(item.name));
+      console.log(BusinessMatch);
+    return BusinessMatch;
+  }
+
   onMarkerClick = (markerProps, marker, e) => {
-    //close any previous infoWindows before opening the next one
+    // close any previous infoWindows before opening the next one
     this.hideInfoWindow();
+
+    // variable declaring foursquare URL that embeds the client ID, secret, and version variables
+    let FS_url = `https://api.foursquare.com/v2/venues/explore?client_id=${FourSquare_CLIENT_ID}&client_secret=${FourSquare_SECRET}&v=${FourSquare_VERSION}&limit=1&ll=${markerProps.position.lat}, ${markerProps.position.lng}, &query=restaurant`
+    let headers = new Headers(); //TODO figure out whether this needs parens
+    // variable declaring a new request that inputs the foursquare URL
+    let request = new Request(FS_url, {
+      method: 'GET',
+      headers
+    });
+    let selectedMarkerProps; 
+    // fetch FourSquare data from API for a restaurant using previously declared request
+    fetch(request)
+      .then(response => response.json())
+      .then(result => {
+        // obtain the individual matching restaurant from
+        console.log('markerProps', markerProps, 'result:', result, 'selectedMarkerProps', selectedMarkerProps)
+        let restaurant = this.findBusinessMatch(markerProps, result);
+        selectedMarkerProps = {
+          fsRestaurant: restaurant[0],
+          ...markerProps
+        }
+      })
+
     marker.setAnimation(window.google.maps.Animation.BOUNCE);
-    //set the state to show marker info (google maps react documentation https://www.npmjs.com/package/google-maps-react) 
+    // credit: for next line of code, with my own modification for second bounce 
+    // https://stackoverflow.com/questions/7339200/bounce-a-pin-in-google-maps-once
+    setTimeout(function(){ marker.setAnimation(null); }, 1400);
+    // set the state to show marker info (google maps react documentation https://www.npmjs.com/package/google-maps-react) 
     this.setState({
       selectedMarker: marker,
       selectedMarkerProps: markerProps,
@@ -53,20 +94,19 @@ class MapContainer extends Component {
   });
   console.log(this.state.selectedMarker, this.state.selectedMarkerProps);
   }
-  //update the marker from the null state to the locations from data
+  // update the marker from the null state to the locations from data
   resetMarkers(locations) {
-    // If no locations exist or they've all been filtered, we're done--credit: https://stackoverflow.com/questions/2647867/how-to-determine-if-variable-is-undefined-or-null
+    // if no locations exist or they've all been filtered, we're done--credit: https://stackoverflow.com/questions/2647867/how-to-determine-if-variable-is-undefined-or-null
     if (!locations) 
       return;
-      console.log(locations);
     
-    //remove any existing markers
+    // remove any existing markers
     this.state.markers.forEach(marker => marker.setMap(null));
 
-    //declare marker properties as an array inside this function
+    // declare marker properties as an array inside this function
     let markerProps = [];
-    //iterate over locations to create markers and properties
-    //Credit this article for using .map instead of forEach (https://jjude.com/react-array/)
+    // iterate over locations to create markers and properties
+    // credit this article for using .map instead of forEach (https://jjude.com/react-array/)
     let markers = locations.map((location, index) => {
       let markerData = {
         key: index,
@@ -77,14 +117,14 @@ class MapContainer extends Component {
       };
       markerProps.push(markerData);
 
-      //declaring new google maps marker (for each mapped location) to have the position of the location data
+      // declaring new google maps marker (for each mapped location) to have the position of the location data
       let marker = new this.props.google.maps.Marker({
         map: this.state.map,
         position: location.pos,
       });
 
       marker.addListener('click', () => {
-        this.onMarkerClick(markerProps, marker, null);
+        this.onMarkerClick(markerData, marker, null);
       });
       return marker;
     });
@@ -117,7 +157,8 @@ class MapContainer extends Component {
           onClose={this.state.hideInfoWindow}
         >
           <div>
-            <h3>Something's wrong with this data {this.state.selectedMarkerProps && this.state.selectedMarkerProps.name}</h3>
+            <h3>{this.state.selectedMarkerProps && this.state.selectedMarkerProps.restaurantName}</h3>
+            <p>Best known for: {this.state.selectedMarkerProps && this.state.selectedMarkerProps.bestKnownFor}</p>
           </div>
         </InfoWindow>
       </Map>
